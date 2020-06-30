85dee4b73de082cf25f3bf9056ada653772d9e82
ikouchiha47 <princeofpersiaa3.ag@gmail.com>
2020-06-30 05:57:44 +0530

    Screen layout

---
+++ .screenlayout/arrangement.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+xrandr --output eDP1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP1 --off --output DP2 --off --output HDMI1 --mode 1920x1080 --pos 2592x0 --rotate normal --output HDMI2 --off --output VIRTUAL1 --off 
+#--transform 1.349991,0.000000,0.000000,0.000000,1.349991,0.000000,0.000000,0.000000,1.000000
